661d217c4129d621c44ed738f260f46cda15f4c1
CarlosMarotoCondato <carlos.maroto.condato@gmail.com>
2022-03-15 13:50:40 +0100

    Router terminado

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InicioTaskComponent } from './componets/task/inicio-task/inicio-task.component';
+import { ListTaskComponent } from './componets/task/list-task/list-task.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'inicio', component: InicioTaskComponent},
+  {path: 'list', component: ListTaskComponent},
+  {path: '', redirectTo: "inicio", pathMatch: "full"}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './componets/header/header.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { InicioTaskComponent } from './componets/task/inicio-task/inicio-task.component';
+import { ListTaskComponent } from './componets/task/list-task/list-task.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    InicioTaskComponent,
+    ListTaskComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
